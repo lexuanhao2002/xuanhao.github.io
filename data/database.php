10827613fd4ff89c3c7a7d8fd1908373a89af335
lexuanhao2002 <98071701+lexuanhao2002@users.noreply.github.com>
2022-04-14 09:26:43 +0700

    Update database.php

--- data/database.php
+++ data/database.php
@@ -1,6 +1,6 @@
 <?php
-define('HOST','localhost');
-define('USERNAME','root');
-define('PASSWORD','');
-define('DATABASE','music');
+define('HOST','ec2-52-3-60-53.compute-1.amazonaws.comt');
+define('USERNAME','bfkeppmyghtvfq');
+define('PASSWORD','213ec408a4ad70654327324ca062cc9bd97423cf17ddcaa1ace554d68bc40a45');
+define('DATABASE','dfbo51migrhs04');
 ?>
